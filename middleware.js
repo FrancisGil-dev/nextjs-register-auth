aed145ce759d16c51df748009f0f39ab0b376adf
Francis.dev <francisgil.dev@gmail.com>
2024-03-18 15:35:18 +0800

    commit

--- middleware.js
+++ middleware.js
@@ -7,6 +7,15 @@ export const middleware = (req) => {
     const url = req.url;
 
     if (!verify && url.includes("/dashboard")) {
-        return NextResponse.redirect('https://nextjs-register-auth.vercel.app/')
+        const local = 'http://localhost:3000';
+        const productionURL = 'https://nextjs-register-auth.vercel.app/';
+
+        if ( url.includes("/nextjs-register-auth.vercel.app")) {
+            return NextResponse.redirect(productionURL);
+        } 
+        else{
+            return NextResponse.redirect(local)
+        }
+        
     }
 }
