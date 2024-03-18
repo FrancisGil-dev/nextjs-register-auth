ac012363dab7684c6eba8b724899356ae20ea380
Francis.dev <francisgil.dev@gmail.com>
2024-03-18 15:27:25 +0800

    commit

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -1,10 +1,8 @@
 import connectDb from "@/utils/db";
 import User from "@/models/Schema";
-import jwt from "jsonwebtoken";
+
 import bcrypt from 'bcryptjs';
 import { NextResponse } from "next/server";
-
-
 
 
 export const POST = async(req) => {
@@ -24,13 +22,10 @@ export const POST = async(req) => {
 
         // if the current password and the hashed password in database is correct
         if (passIsMatch) {
-            // create a token
-            const token = jwt.sign({email: findUser.email},process.env.JWT_SECRET || 'r32bsVKnvjFV');
-            
-             
             // response it to client
-            return new NextResponse(token, {status: 200});
+            return new NextResponse("Successfully Login...", {status: 200});
         }
+        
         else{
             return new NextResponse("Invalid Email and Password...", {status: 401});
         }
